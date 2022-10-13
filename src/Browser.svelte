e8747f99fff5f713227f894ca22377236c66494e
breadthe <breadthe@users.noreply.github.com>
2022-10-13 09:26:55 -0500

    Add svelte-splitpanes

--- src/Browser.svelte
+++ src/Browser.svelte
@@ -1,16 +1,29 @@
 <script lang="ts">
   // system/lib/util imports
+  import { Pane as SplitPane, Splitpanes } from "svelte-splitpanes"
 
   // store imports
   //   import { system } from "./store"
   //   const { activeSection } = system
 
   // component imports
-//   import Runs from "./lib/Runs.svelte"
 </script>
 
-<!-- <Runs template="gallery" /> -->
-Browser
+<Splitpanes class="default-theme" style="">
+  <SplitPane minSize={40}>Left Pane</SplitPane>
+  <SplitPane minSize={40}>Right Pane</SplitPane>
+</Splitpanes>
 
 <style>
+  :global(.splitpanes) {
+    height: calc(100vh - 120px);
+  }
+  :global(.splitpanes__pane) {
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.2) inset;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    position: relative;
+    height: 100%;
+  }
 </style>
