bed78414f46a3b455fa4c44d56414be9b5a51155
breadthe <breadthe@users.noreply.github.com>
2022-10-13 11:37:13 -0500

    Folder styling

--- src/lib/icons/Folder.svelte
+++ src/lib/icons/Folder.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 </script>
 
-<svg
-  viewBox="0 0 15 15"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-  width="15"
-  height="15"
-  ><path
-    d="M.5 12.5v-10a1 1 0 011-1h4l2 2h6a1 1 0 011 1v8a1 1 0 01-1 1h-12a1 1 0 01-1-1z"
-    stroke="currentColor"
-  /></svg
->
+<div class="text-yellow-400">
+    <svg
+    viewBox="0 0 15 15"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    width="15"
+    height="15"
+    ><path
+      d="M.5 12.5v-10a1 1 0 011-1h4l2 2h6a1 1 0 011 1v8a1 1 0 01-1 1h-12a1 1 0 01-1-1z"
+      stroke="gray"
+    /></svg
+  >
+</div>
