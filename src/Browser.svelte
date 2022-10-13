be4bd6306a14f5c0f7194ffadeaef84ced013c4d
breadthe <breadthe@users.noreply.github.com>
2022-10-13 11:48:24 -0500

    Add bottom command bar

--- src/Browser.svelte
+++ src/Browser.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import CommandBar from "./lib/CommandBar.svelte"
+
   // system/lib/util imports
   import { Pane as SplitPane, Splitpanes } from "svelte-splitpanes"
 
@@ -15,9 +17,11 @@
   <SplitPane minSize={40}><Pane pane="right" /></SplitPane>
 </Splitpanes>
 
+<CommandBar />
+
 <style>
   :global(.splitpanes) {
-    height: calc(100vh - 120px);
+    height: calc(100vh - 100px);
   }
   :global(.splitpanes__pane) {
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.2) inset;
---
+++ src/lib/CommandBar.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  // system/lib/util imports
+
+  // store imports
+  //   import { system } from "./store"
+  //   const { activeSection } = system
+
+  // component imports
+</script>
+
+<div class="grid grid-cols-6 justify-between">
+    <button>View - F3</button>
+    <button>Edit - F4</button>
+    <button>Copy - F5</button>
+    <button>Move - F6</button>
+    <button>New Folder - F7</button>
+    <button>Delete - F8</button>
+</div>
+
+<style>
+    button {
+        cursor: theme("cursor.auto");
+        font-size: theme("fontSize.sm");
+        background-color: theme("colors.gray.100");
+        border: 1px solid theme("colors.gray.300");
+        padding: theme("padding.1") theme("padding.2");
+    }
+
+    button:hover {
+        background-color: theme("colors.gray.200");
+    }
+</style>
